9cf7f93613d662bc87ea39a2c6208be3419c2599
kevkim01 <kkim068@ucr.edu>
2018-01-02 20:25:50 -0800

    updated project skeleton

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCFvuKy_wxFZECoVfxHdTEA6wforkzPpak",
+    authDomain: "fitness-3bdd3.firebaseapp.com",
+    databaseURL: "https://fitness-3bdd3.firebaseio.com",
+    projectId: "fitness-3bdd3",
+    storageBucket: "",
+    messagingSenderId: "1013466222784"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCFvuKy_wxFZECoVfxHdTEA6wforkzPpak",
+    authDomain: "fitness-3bdd3.firebaseapp.com",
+    databaseURL: "https://fitness-3bdd3.firebaseio.com",
+    projectId: "fitness-3bdd3",
+    storageBucket: "",
+    messagingSenderId: "1013466222784"
+  }
 };
