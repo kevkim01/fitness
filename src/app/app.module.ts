edc9c44f73da8114dbdddc385294d10851d7ec03
kevkim01 <kkim068@ucr.edu>
2018-01-04 20:08:45 -0800

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,44 @@ import { AppComponent } from './app.component';
 
 import { environment } from './../environments/environment';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { AccountComponent } from './account/account.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'account', component: AccountComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full'}
+]
+
+export const firebaseCofig = {
+  apiKey: "AIzaSyCFvuKy_wxFZECoVfxHdTEA6wforkzPpak",
+  authDomain: "fitness-3bdd3.firebaseapp.com",
+  databaseURL: "https://fitness-3bdd3.firebaseio.com",
+  projectId: "fitness-3bdd3",
+  storageBucket: "",
+  messagingSenderId: "1013466222784"
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    HomeComponent,
+    SignInComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
